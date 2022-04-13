4e19900fc27997816de7ce1bffa10cb88b55c3b3
vaibhav19972 <100220382+vaibhav19972@users.noreply.github.com>
2022-04-13 18:52:23 +0530

    updated css
    made changes to css and minimized javascript

--- exe/Project2/index.js
+++ exe/Project2/index.js
@@ -4,44 +4,16 @@ var panel3 = document.getElementById('panel-3')
 var panel4 = document.getElementById('panel-4')
 var panel5 = document.getElementById('panel-5')
 
-function fun1(){
-    var re=getactive()
-    re.classList.remove('active')
-    panel1.classList.add('active'); 
+function selectPanel(panelname){
+      removeactive()
+      panelname.classList.add('active')
+      changebg(panelname)
+    }
+function removeactive(){
+      panel1.classList.remove('active') 
+      panel2.classList.remove('active') 
+      panel3.classList.remove('active') 
+      panel4.classList.remove('active') 
+      panel5.classList.remove('active') 
 }
-function fun2(){
-    var re=getactive()
-    re.classList.remove('active')
-    panel2.classList.add('active'); 
-    
 
-}
-function fun3(){
-    var re=getactive()
-    re.classList.remove('active')
-    panel3.classList.add('active'); 
-}
-function fun4(){
-    var re=getactive()
-    re.classList.remove('active')
-    panel4.classList.add('active'); 
-}
-function fun5(){
-    var re=getactive()
-    re.classList.remove('active')
-    panel5.classList.add('active'); 
-}
-function getactive(){
-    var activepanel 
-    if(panel1.classList.contains('active'))
-          activepanel=panel1
-    if(panel2.classList.contains('active'))
-          activepanel=panel2
-    if(panel3.classList.contains('active'))
-          activepanel=panel3
-    if(panel4.classList.contains('active'))
-          activepanel=panel4
-    if(panel5.classList.contains('active'))
-         activepanel=panel5
-    return activepanel
-}
