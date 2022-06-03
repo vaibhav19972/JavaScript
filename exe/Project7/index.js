399ad120b26c278faeaf3adb871e3a3db432229e
vaibhav19972 <100220382+vaibhav19972@users.noreply.github.com>
2022-06-03 18:44:11 +0530

    added validations for signup
    various validate functions

--- exe/Project7/index.js
+++ exe/Project7/index.js
@@ -43,8 +43,75 @@ let helpNav = document.getElementById('helpNav');
 			break
         case 'Help':
             helpNav.classList.add('active')
-            helpPage.style.diaplay = 'block'
-            console.log('help page')
+            helpPage.style.display = 'block'
+            console.log('help p age')
 
 	}
 }
+/**
+ * validate
+ */
+ let invalidName = document.getElementById("invalidName")
+ let emailInvalid = document.getElementById("invalidSEmail")
+ let phoneInvalid = document.getElementById("invalidPhone")
+ let stateInvalid = document.getElementById("invalidState")
+ let zipCodeInvalid = document.getElementById("invalidZip")
+ let vaildCheck = document.getElementById("invalidCheck")
+
+ function validate() {
+	let name = document.getElementById("name").value;
+	let email = document.getElementById("Semail").value;
+	let phoneNumber = document.getElementById("phone").value;
+	let state = document.getElementById("state").value;
+	let zipCode = document.getElementById("zip").value;
+	let invalidCheck = document.getElementById("invalidCheck");
+	console.log("hi")
+	let ct=0;
+  
+	if (name === ''){
+	  invalidName.style.display = 'block'
+	} else {
+		invalidName.style.display = 'none'
+	  ct++
+	}
+  
+	if(email === '' || !email.includes('@') || !email.includes('.') || email.startsWith('@') || email.slice(email.lastIndexOf('.') + 1).length < 2) {
+	  emailInvalid.style.display = 'block'
+	} else {
+	  emailInvalid.style.display = 'none'
+	  ct++
+	}
+  
+	if(phoneNumber === '' || phoneNumber.length !=10){
+	  phoneInvalid.style.display = 'block'
+	} else {
+		phoneInvalid.style.display = 'none'
+	  ct++
+	}
+  
+	if(state === '' ){
+	  stateInvalid.style.display = 'block'
+	} else {
+		stateInvalid.style.display = 'none'
+	  ct++
+	}
+  
+	if(zipCode === '' || zipCode.length != 6 ){
+	  zipCodeInvalid.style.display = 'block'
+	} else {
+		zipCodeInvalid.style.display = 'none'
+	  ct++
+	}
+  
+	if( !invalidCheck.checked ){
+	  vaildCheck.style.display = 'block'
+	} else {
+		vaildCheck.style.display = 'none'
+	  ct++
+	}
+    if(ct === 6){
+		return true;
+	}else{
+		return false;
+	}
+  }
